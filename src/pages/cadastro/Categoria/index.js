0162ea9b95ee96dc94f1c6cf495316c162b36187
WeslleyFratini <weeslleey354@gmail.com>
2020-07-28 16:35:55 -0300

    mudanças finais

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,20 @@ function CadastroCategoria() {
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
+      <form>
+
+        <label>
+          Nome da Categoria
+          <input
+          type="Text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
 
 
       <Link to="/">
